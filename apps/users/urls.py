d3882df38a8d5ec2cabde574926dc37200205447
Arsen <114998508+arsenn5@users.noreply.github.com>
2023-11-02 22:02:44 +0600

    add profile users

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from apps.users.views import RegisterView, LoginView
+from apps.users.views import RegisterView, LoginView, questionnaire_view, service_view
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
+    path('profile/', questionnaire_view),
+    path('service/', service_view)
 ]
